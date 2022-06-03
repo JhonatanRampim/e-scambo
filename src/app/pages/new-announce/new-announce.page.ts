da79fc1b8362d0b1ed2828c115c96a63f98cd484
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2022-06-02 22:09:55 -0300

    improvements in form

--- src/app/pages/new-announce/new-announce.page.ts
+++ src/app/pages/new-announce/new-announce.page.ts
@@ -13,11 +13,18 @@ export class NewAnnouncePage implements OnInit {
 
   ngOnInit() {
     this.novoMovelForm  = this.formBuilder.group({
-      nome: [''],
-      descricao: [''],
-      estado: [''],
-      basicfile:['']
+      nome: ['',[Validators.required]],
+      descricao: ['',[Validators.required]],
+      estado: ['',[Validators.required]],
+      foto1:['', [Validators.required]],
+      foto2:['', [Validators.required]],
+      foto3:[''],
+      foto4:[''],
     })
+  }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.novoMovelForm.value);
   }
 
 }
